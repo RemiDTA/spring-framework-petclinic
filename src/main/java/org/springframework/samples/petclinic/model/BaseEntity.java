62b1803b1096e041fd9eecacc715be3522b1f02e
Remi <ferreira-remi@hotmail.fr>
2018-01-17 12:11:44 +0100

    Retrait de l'Erreur

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -31,7 +31,7 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
-	a que salut
+
 
     public Integer getId() {
         return id;
